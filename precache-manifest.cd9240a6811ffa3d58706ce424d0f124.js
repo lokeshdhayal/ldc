38213e3da34156fc2fed7a9654e40eeace8306f2
Lokesh Dhayagl <lokeshdhayalcharanwas@gmail.com>
2020-08-01 17:07:34 +0530

    Updates

---
+++ precache-manifest.cd9240a6811ffa3d58706ce424d0f124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba6d2ed341012cc1ed362ed5e5ffcd4",
+    "url": "/ldc/index.html"
+  },
+  {
+    "revision": "5de79e0c3383d3f1cb22",
+    "url": "/ldc/static/js/2.244a980d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ldc/static/js/2.244a980d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7373b3c6a27c5c527090",
+    "url": "/ldc/static/js/main.f0fd7c60.chunk.js"
+  },
+  {
+    "revision": "ebdbd99b89954831d95e",
+    "url": "/ldc/static/js/runtime-main.ce94b7de.js"
+  }
+]);
