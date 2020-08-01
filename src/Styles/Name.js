62507bb92f9686a49288140e5df33572e8c1a90b
Lokesh Dhayagl <lokeshdhayalcharanwas@gmail.com>
2020-08-01 20:11:03 +0530

    Lokesh1

--- src/Styles/Name.js
+++ src/Styles/Name.js
@@ -20,12 +20,12 @@ class Name extends React.Component {
     render(){return (
         <div>
         <div style={{display:"flex",justifyContent:"center",height:`${this.state.height}vh`,alignItems:"center"}}> 
-            <h1 style={{color:"green",fontSize:125,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>L</h1>
-            <h1 style={{color:"tomato",fontSize:125,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>O</h1>
-            <h1 style={{color:"teal",fontSize:125,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>O</h1>
-            <h1 style={{color:"aqua",fontSize:125,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>S</h1>
-            <h1 style={{color:"yellow",fontSize:125,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>E</h1>
-            <h1 style={{color:"blue",fontSize:125,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>R</h1>
+            <h1 style={{color:"green",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>L</h1>
+            <h1 style={{color:"tomato",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>O</h1>
+            <h1 style={{color:"teal",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>O</h1>
+            <h1 style={{color:"aqua",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>S</h1>
+            <h1 style={{color:"yellow",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>E</h1>
+            <h1 style={{color:"blue",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>R</h1>
             </div>
             <div style={{display:"flex",justifyContent:"center",height:`${30}vh`,alignItems:"center"}}>
         {this.state.timer?<button className="btn btn-primary" style={{fontSize:25,padding:5}}>Get start</button>:''}
