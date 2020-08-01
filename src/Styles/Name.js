72eb08760646bde0a56bacb08482abd9f9bf4a5e
Lokesh Dhayagl <lokeshdhayalcharanwas@gmail.com>
2020-08-01 21:57:58 +0530

    Lokesh2

--- src/Styles/Name.js
+++ src/Styles/Name.js
@@ -19,7 +19,7 @@ class Name extends React.Component {
     }
     render(){return (
         <div>
-        <div style={{display:"flex",justifyContent:"center",height:`${this.state.height}vh`,alignItems:"center"}}> 
+        <div className="w3-animate-top" style={{display:"flex",justifyContent:"center",height:`${this.state.height}vh`,alignItems:"center"}}> 
             <h1 style={{color:"green",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>L</h1>
             <h1 style={{color:"tomato",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>O</h1>
             <h1 style={{color:"teal",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>O</h1>
@@ -28,7 +28,7 @@ class Name extends React.Component {
             <h1 style={{color:"blue",fontSize:100,fontFamily:"Comic Sans MS, cursive, sans-serif"}}>R</h1>
             </div>
             <div style={{display:"flex",justifyContent:"center",height:`${30}vh`,alignItems:"center"}}>
-        {this.state.timer?<button className="btn btn-primary" style={{fontSize:25,padding:5}}>Get start</button>:''}
+        {this.state.timer?<button className="w3-btn w3-yellow w3-animate-zoom w3-hover-teal" style={{fontSize:25,padding:15}}>Get start<span style={{margin:10}} className="fa fa-arrow-right"></span></button>:''}
         </div>
         </div>
     )}
