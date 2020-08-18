e1522225c08e8420c5bec765999fa54156ec7f22
Lokesh Dhayagl <lokeshdhayalcharanwas@gmail.com>
2020-08-19 00:18:19 +0530

    dfjsfhsf

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,12 @@ const Footer = () => {
     return (
         <footer class="page-footer font-small w3-purple">
             <div className = "container-fluid row">
-                <div className = "col-6 p-2">
+            <div style = {{width:"500px",maxWidth:"100%",padding:30}}>
                     <p>We are very Happy tha tyou visit our website.This website is made on Laptop so if you find any difficult in using this at Phone.Please let's Know.</p>
                     <p className = "w3-text-aqua">Please Mention your Feedback in Help Section</p>
                     <Link to = "/help" className = "btn btn-info">Feedback</Link>
                 </div>
-                <div className = "col-6">
+                <div style = {{width:"500px",maxWidth:"100%",margin:10}}>
                     <p>Contact Detail</p>
                     <p><i className = "fa fa-instagram"  style = {{fontSize:40,color:"red",margin:10}} aria-hidden="true"></i>Instagram:Not Available</p>
                     <p><i className = "fa fa-facebook"  style = {{fontSize:40,color:"blue",margin:10}} aria-hidden="true"></i>FaceBook:Not Available</p>
