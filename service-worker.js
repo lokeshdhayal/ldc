46ec7481ecfbd0e8679eccb203ae56e825ad2ff4
Lokesh Dhayagl <lokeshdhayalcharanwas@gmail.com>
2020-08-19 12:26:01 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ldc/precache-manifest.becd5f349f1883e656d050b6488bef7c.js"
+  "/ldc/precache-manifest.dc487a45eaf9707d2b73461d36d5485b.js"
 );
 
 self.addEventListener('message', (event) => {
