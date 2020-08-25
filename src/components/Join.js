8057eec1d920b5d9f8f94c4f6724601551f86103
Lokesh Dhayagl <lokeshdhayalcharanwas@gmail.com>
2020-08-25 20:05:02 +0530

    sdg

--- src/components/Join.js
+++ src/components/Join.js
@@ -8,9 +8,9 @@ const Join = () => {
         <div className ="row" style = {{display:"flex",justifyContent:"center",alignItems:"center"}}>
         <div className = "card col-6 m-3 p-3">
             <label>Enter Name:</label>
-            <input type = "text" onChange = {(e)=>setName(e.target.value)} className = "form-control" />
-            <label>Emter room:</label>
             <input type = "text" onChange = {(e) => setRoom(e.target.value)}  className = "form-control"/>
+            <label>Emter room:</label>
+            <input type = "text" onChange = {(e)=>setName(e.target.value)} className = "form-control" />
             <Link to = {`/chat?user=${room}&room=${name}`} className ="btn btn-block btn-info m-2">Sign In</Link>
         </div>
         </div>
--- src/components/msg.js
+++ src/components/msg.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ReactEmoji from 'react-emoji';
+
 
 const msg  = (props) => {
     if(props.msg.user !== "me"){
     return (
         <div key = {Math.random()} stlye = {{display:"flex"}}>
-            <p className = "w3-padding w3-blue" style = {{borderRadius:30,maxWidth:"70%"}}>{props.msg.text}<span className = "w3-pink badge w3-right">{props.msg.user}</span></p>
+            <p className = "w3-padding w3-blue" style = {{borderRadius:30,maxWidth:"70%"}}>{ReactEmoji.emojify(props.msg.text)}<span className = "w3-pink badge w3-right">{props.msg.user}</span></p>
             </div>)
     }
     else{
@@ -14,7 +16,7 @@ const msg  = (props) => {
 
                 </div>
                 <div style = {{flex:5}}>
-                <p className = "w3-padding w3-red" style = {{width:"auto",borderRadius:30,justifyContent:"flex-end"}}>{props.msg.text}</p>
+                <p className = "w3-padding w3-red" style = {{width:"auto",borderRadius:30,justifyContent:"flex-end"}}>{ReactEmoji.emojify(props.msg.text)}</p>
                 </div>
                 </div>)
     }
